5b330008cb5788bf10f5afc113f36107d72bb8eb
Danielle Barker - Mac <caseybarker86@gmail.com>
2022-11-02 12:24:43 -0600

    Added Adder and Displayer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
 import { useState } from "react";
 import "./App.css";
+import Adder from "./components/Adder";
+import Displayer from "./components/Displayer";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [name, setName] = useState("Danielle");
-  function increaseCount(amount = 1) {
-    setCount(count + amount);
+  const [count2, setCount2] = useState(0);
+
+  function increaseCount() {
+    setCount((curr) => {
+      return curr + 1;
+    });
   }
 
   return (
     <div className="App">
       <header className="App-header">
         {count}
-        <button onClick={() => increaseCount(1)}>Increase Count</button>
-        <button onClick={() => increaseCount(2)}>Increase Count By Two</button>
-        <button onClick={() => increaseCount()}>
-          Increase Count By Unknown
+        <button
+          style={{
+            backgroundColor: "orange",
+            padding: "10px",
+            borderRadius: "10px",
+          }}
+          onClick={() => increaseCount()}
+        >
+          Increase Count
         </button>
+        <Displayer count={count2} />
+        <Adder setCount={setCount2} />
       </header>
     </div>
   );
