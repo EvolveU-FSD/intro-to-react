c1a4fae1587e7209f5ae6ebc471a5509a81a474f
Danielle Barker - Mac <caseybarker86@gmail.com>
2022-11-02 11:27:22 -0600

    Added a count variable and button to increase

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("Danielle");
+  function increaseCount(amount = 1) {
+    setCount(count + amount);
+  }
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        {count}
+        <button onClick={() => increaseCount(1)}>Increase Count</button>
+        <button onClick={() => increaseCount(2)}>Increase Count By Two</button>
+        <button onClick={() => increaseCount()}>
+          Increase Count By Unknown
+        </button>
+      </header>
     </div>
   );
 }
